393f49002c0a35442c9f2f2b6b4bb02a8b8d28ac
Qxtdl <230689417+Qxtdl@users.noreply.github.com>
2025-10-04 20:53:13 +0000

    make it read source code

---
+++ src/lexer/lexer.h
@@ -0,0 +1,22 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+typedef enum {
+    L_PARAN,
+    R_PARAN,
+    L_BRACE,
+    R_BRACE,
+    TOKEN_ASSIGN,      // =
+    TOKEN_EQUALS,      // ==
+    TOKEN_PLUS,        // +
+    TOKEN_MINUS,       // -
+    TOKEN_STAR,        // *
+    TOKEN_SLASH,       // /
+    TOKEN_SEMICOLON,   // ;
+    TOKEN_COMMA,       // ,
+    TOKEN_IDENTIFIER,  // variable/function names
+    TOKEN_NUMBER,      // numeric literals
+    TOKEN_EOF  // end of input
+} token_t;
+
+#endif // LEXER_H
