e0126a94cb2ed050055456dfcc48cca1a7e862dc
yu34567890 <133307919+yu34567890@users.noreply.github.com>
2025-10-05 00:20:18 +0300

    Update lexer.c

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,1 +1,21 @@
+#include <stdbool.h>
 #include "lexer.h"
+
+static struct {
+    const char *source;
+} lexer_state;
+
+static void lexer_push()
+{
+    
+}
+
+void lexer_init(const char *source)
+{
+    lexer_state.source = source;
+}
+
+bool lexer_next_token()
+{
+
+}
